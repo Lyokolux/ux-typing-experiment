266f5442fb3964ee9b935c67eea125ced5da333c
johannchopin <johannchopin@pm.me>
2021-09-21 22:23:59 +0200

    💩 Test localize in Home

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import { _ } from 'svelte-i18n'
   import { swiper } from '../stores'
 </script>
 
+<h1>{$_('title')}</h1>
 <h2>Home</h2>
 <button class="btn btn-primary" on:click={() => $swiper.slideNext() }>next</button>
