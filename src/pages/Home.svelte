8bd374ed90dd1820b6e9da8342acebf96891cb27
johannchopin <johannchopin@pm.me>
2021-09-21 22:59:43 +0200

    💩 Add dummy Questions in Home

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n'
   import { swiper } from '../stores'
+
+  import QuestionsForm from '../components/QuestionsForm/QuestionsForm.svelte'
 </script>
 
 <h1>{$_('title')}</h1>
 <h2>Home</h2>
 <button class="btn btn-primary" on:click={() => $swiper.slideNext() }>next</button>
+
+<QuestionsForm 
+  questions={[
+    {
+      labels: ['Déplaisante', 'Plaisante'],
+      inverted: true,
+      grade: null
+    },
+    {
+      labels: ['Déplaisante', 'Plaisante'],
+      grade: null
+    },
+    {
+      labels: ['pas cool', 'cool'],
+      grade: 4
+    }
+  ]}
+/>
