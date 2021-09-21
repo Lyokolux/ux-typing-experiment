21c1516ef810cf2d43093d41427ea09d51508eb2
johannchopin <johannchopin@pm.me>
2021-09-21 22:23:17 +0200

    🌐 Init i18n

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,13 @@
-<script>
+<script context="module" lang="ts">
+  import '../i18n' 
+  import { waitLocale } from 'svelte-i18n'
+
+  export async function load() {
+    await waitLocale()
+    return {}
+  }
+</script>
+<script lang="ts">
   import '../global.scss'
 </script>
 
