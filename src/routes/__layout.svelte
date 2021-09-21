055ac06b83b54fc640a5ae510411b8971a6f9f53
johannchopin <johannchopin@pm.me>
2021-09-21 16:52:38 +0200

    💄 Better use dark theme

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,7 @@
   import '../global.scss'
 </script>
 
-<div class="swiper w-100 h-100 bg-light">
+<div class="swiper w-100 h-100 bg-dark text-light">
   <div class="swiper-wrapper">
     <slot></slot>
   </div>
