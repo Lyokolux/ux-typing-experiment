6ac486d2f573663d98e8f48730d2d6fb70d89d53
johannchopin <johannchopin@pm.me>
2021-09-21 22:59:19 +0200

    ✨ QuestionsForm component

---
+++ src/components/QuestionsForm/QuestionsForm.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+  export interface Question {
+    labels: string[] // like ['Plaisante', 'Déplaisante']
+    // the labels are inverted
+    inverted?: boolean
+    grade: number | null
+  }
+</script>
+<script lang="ts">
+  import QuestionComponent from "./Question.svelte";
+
+  export let questions: Question[]
+
+  $: console.log(questions)
+</script>
+
+<table>
+  {#each questions as question}
+    <QuestionComponent bind:question />
+  {/each}
+</table>
