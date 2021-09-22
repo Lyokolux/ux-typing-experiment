5eb0662e5ba4b58b02e6ba9291ac392c9e5e69a9
johannchopin <johannchopin@pm.me>
2021-09-22 23:21:56 +0200

    ♻️ Move sexe question in component

--- src/pages/UserInfos/UserInfos.svelte
+++ src/pages/UserInfos/UserInfos.svelte
@@ -2,33 +2,16 @@
   import * as yup from 'yup'
   import { _ } from 'svelte-i18n'
 
+  import SexeQuestion, { SEXES } from './questions/Sexe.svelte'
+  import type { Sexe } from './questions/Sexe.svelte'
   import FormErrors from './FormErrors.svelte'
 
   const AGE_RANGES = ['-15', '15-17', '18-24', '25-34', '35-44', '45-54', '55-64', '+65'] as const
-  const SEXES = ['m', 'f', 'no-answer', 'else'] as const
 
   interface UserInfos {
-    sexe: typeof SEXES[number]
+    sexe: Sexe
     age:  typeof AGE_RANGES[number]
   }
-  const SEXE_QUESTION: { value: UserInfos['sexe'], label: string }[] = [
-    {
-      value: 'm',
-      label: $_('user_infos.sexe.male')
-    },
-    {
-      value: 'f',
-      label: $_('user_infos.sexe.female')
-    },
-    {
-      value: 'else',
-      label: $_('user_infos.sexe.else')
-    },
-    {
-      value: 'no-answer',
-      label: $_('user_infos.sexe.no_answer')
-    }
-  ]
 
   let userInfos: Partial<UserInfos> = {}
   let errors: string[] = []
@@ -50,20 +33,7 @@
 
 <form on:submit|preventDefault={onSubmit}>
   <fieldset class="d-flex flex-column">
-    <h3>{$_('user_infos.sexe.you_are')}:</h3>
-    {#each SEXE_QUESTION as { value, label }}
-      <label>
-        <input 
-          type="radio" 
-          class="form-check-input" 
-          name="user_sexe" 
-          value={value}
-          required
-          bind:group={userInfos.sexe}
-        >
-        {label}
-      </label>
-    {/each}
+    <SexeQuestion bind:sexe={userInfos.sexe} />
   </fieldset>
 
   <FormErrors {errors} />
---
+++ src/pages/UserInfos/questions/Sexe.svelte
@@ -0,0 +1,43 @@
+<script lang="ts" context="module">
+  export const SEXES = ['m', 'f', 'no-answer', 'else'] as const
+  export type Sexe = typeof SEXES[number]
+</script>
+<script lang="ts">
+  import { _ } from 'svelte-i18n'
+
+  export let sexe: Sexe
+
+  const SEXE_QUESTION: { value: Sexe, label: string }[] = [
+    {
+      value: 'm',
+      label: $_('user_infos.sexe.male')
+    },
+    {
+      value: 'f',
+      label: $_('user_infos.sexe.female')
+    },
+    {
+      value: 'else',
+      label: $_('user_infos.sexe.else')
+    },
+    {
+      value: 'no-answer',
+      label: $_('user_infos.sexe.no_answer')
+    }
+  ]
+</script>
+
+<h3>{$_('user_infos.sexe.you_are')}:</h3>
+{#each SEXE_QUESTION as { value, label }}
+  <label>
+    <input 
+      type="radio" 
+      class="form-check-input" 
+      name="user_sexe" 
+      value={value}
+      required
+      bind:group={sexe}
+    >
+    {label}
+  </label>
+{/each}
