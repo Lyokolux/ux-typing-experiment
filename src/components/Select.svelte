26236b44f77e21e47a25bb5663e98c3d3cb1e837
johannchopin <johannchopin@pm.me>
2021-09-22 23:21:06 +0200

    ✨ Select also accept values as object

--- src/components/Select.svelte
+++ src/components/Select.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
   import type { SvelteComponent } from 'svelte'
 
+  interface Value {
+    value: string
+    label: string
+  }
+
   let className = ''
   export { className as class }
   export let value: string | undefined
-  export let values: string[]
+  export let values: string[] | Value[]
   export let icon: SvelteComponent | undefined
+
+  const getValues = (): Value[] => {
+    if (typeof values[0] === 'string') {
+      return values.map((value) => {
+        return {
+          value,
+          label: value
+        }
+      })
+    }
+
+    return values as Value[]
+  }
 </script>
 
 <div 
@@ -13,8 +31,8 @@
   title="Update language"
 >
   <select bind:value={value} class="opacity-0 p-0 position-absolute w-100 h-100">
-    {#each values as option}
-      <option value={option}>{option}</option>
+    {#each getValues() as { value, label }}
+      <option {value}>{label}</option>
     {/each}
   </select>
 
