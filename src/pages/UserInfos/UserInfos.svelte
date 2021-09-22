71a0715d4bdc3939e042328e23e17078458ce211
johannchopin <johannchopin@pm.me>
2021-09-22 23:45:58 +0200

    ✨ Age question

--- src/pages/UserInfos/UserInfos.svelte
+++ src/pages/UserInfos/UserInfos.svelte
@@ -4,20 +4,21 @@
 
   import SexeQuestion, { SEXES } from './questions/Sexe.svelte'
   import type { Sexe } from './questions/Sexe.svelte'
+  import AgeQuestion, { AGES } from './questions/Age.svelte'
+  import type { Age } from './questions/Age.svelte'
   import FormErrors from './FormErrors.svelte'
-
-  const AGE_RANGES = ['-15', '15-17', '18-24', '25-34', '35-44', '45-54', '55-64', '+65'] as const
 
   interface UserInfos {
     sexe: Sexe
-    age:  typeof AGE_RANGES[number]
+    age:  Age
   }
 
   let userInfos: Partial<UserInfos> = {}
   let errors: string[] = []
 
   const schema = yup.object().shape({
-    sexe: yup.mixed().oneOf([...SEXES]).required($_('user_infos.sexe.is_required'))
+    sexe: yup.mixed().oneOf([...SEXES]).required($_('user_infos.sexe.is_required')),
+    age: yup.mixed().oneOf([...AGES]).required($_('user_infos.age.is_required')),
   })
 
   const onSubmit = (): void => {
@@ -34,6 +35,10 @@
 <form on:submit|preventDefault={onSubmit}>
   <fieldset class="d-flex flex-column">
     <SexeQuestion bind:sexe={userInfos.sexe} />
+  </fieldset>
+
+  <fieldset class="d-flex flex-column mt-3">
+    <AgeQuestion bind:age={userInfos.age} />
   </fieldset>
 
   <FormErrors {errors} />
