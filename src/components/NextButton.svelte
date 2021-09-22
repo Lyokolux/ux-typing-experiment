7c7e2493cd99c613f8259f6341acf953773a2667
Lyokolux <lyokolux@protonmail.com>
2021-09-22 17:19:02 +0200

    ✨(ExperienceBeginning.svelte): implement basic screen #9

---
+++ src/components/NextButton.svelte
@@ -0,0 +1,9 @@
+<div class="row justify-center mt-8">
+    <button class="btn btn-primary my-auto" on:click={() => $swiper.slideNext() }>
+        <slot></slot>
+    </button>
+</div>
+
+<script>
+    import { swiper } from '../stores'
+</script>
