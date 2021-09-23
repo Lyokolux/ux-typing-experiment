7fd85c8b4a768d32e03087bde63a56740f822cc4
johannchopin <johannchopin@pm.me>
2021-09-23 11:32:28 +0200

    🔧 Eslint extends airbnb-base/legacy

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	extends: "airbnb-base/legacy",
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
