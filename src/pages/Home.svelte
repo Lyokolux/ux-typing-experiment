d76edddb762431f59f21ea4ba9dd4abd146d1bbb
Lyokolux <lyokolux@protonmail.com>
2021-09-23 15:06:27 +0200

    🚨(Home.svelte)

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n'
-  import NextButton from '../components/NextButton.svelte';
+  import NextButton from '../components/NextButton.svelte'
 
   const PARAGRAPH_AMOUNT = 6
   const I18N_KEYS = 'home.paragraph-'
-  const PARAGRAPHS_KEYS = Array(PARAGRAPH_AMOUNT).fill(undefined).map((_, i) => `${I18N_KEYS}${i+1}`);
+  const PARAGRAPHS_KEYS = Array(PARAGRAPH_AMOUNT).fill(undefined).map((_, i) => `${I18N_KEYS}${i + 1}`)
 </script>
 
 <h1>{$_('title')}</h1>
