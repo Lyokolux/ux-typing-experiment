5d7883d491fd81aa51c8fd8b919cb3d3706e78ef
johannchopin <johannchopin@pm.me>
2021-09-23 16:37:04 +0200

    🎨 Import in Home

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n'
-  import NextButton from '../components/NextButton.svelte'
 
+  import NextButton from '../components/NextButton.svelte'
   import AlphanumericInput from '../components/AlphanumericInput/AlphanumericInput.svelte'
+
   const PARAGRAPH_AMOUNT = 6
   const I18N_KEYS = 'home.paragraph_'
   const PARAGRAPHS_KEYS = Array(PARAGRAPH_AMOUNT).fill(undefined).map((__, i) => `${I18N_KEYS}${i + 1}`)
