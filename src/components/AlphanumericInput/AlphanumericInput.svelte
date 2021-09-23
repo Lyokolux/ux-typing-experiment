bf1ecabd538641954722d4214a7639f3a5ee4692
johannchopin <johannchopin@pm.me>
2021-09-23 15:26:14 +0200

    ✨ Use UX friendly font for AlphanumericInput

--- src/components/AlphanumericInput/AlphanumericInput.svelte
+++ src/components/AlphanumericInput/AlphanumericInput.svelte
@@ -2,14 +2,14 @@
   import { chunk, isNumeric, removeAllNonNumeric } from '../../utils'
   import { KEY } from '../../const'
 
-  export let number: string
+  export let value: string
   export let chunkLength: number
   let className = ''
   export { className as class }
   export let chunkClass: string = ''
   export let onSuccess: () => void
 
-  let chunks: string[] = chunk(number, chunkLength)
+  let chunks: string[] = chunk(value, chunkLength)
   let chunksRef: HTMLInputElement[] = []
   let enteredChunks: string[] = Array(chunks.length).join('.').split('.') // Create array of n empty strings
 
@@ -40,22 +40,21 @@
 
   const checkChunk = (i: number): void => {
     const chunckValue = enteredChunks[i]
-  
-    if (!isNumeric(chunckValue)) {
-      enteredChunks[i] = removeAllNonNumeric(enteredChunks[i])
-    }
+
+    enteredChunks[i] = enteredChunks[i].toUpperCase()
+
     if (chunckValue.length > chunkLength) {
       enteredChunks[i] = chunckValue.substring(0, chunkLength)
     }
   }
 </script>
 
-<div class={`numeric-input ${className}`}>
+<div class={`numeric-input d-flex ${className}`}>
   {#each chunks as digits, i}
     <input
       type="text"
-      class={`chunk form-control m-2 text-center ${chunkClass}`}
-      style={`--width: ${chunkLength + 1}em`}
+      class={`chunk form-control m-1 text-center fs-5 fw-bold ${chunkClass}`}
+      style={`--width: ${chunkLength + .7}em`}
       bind:this={chunksRef[i]}
       bind:value={enteredChunks[i]}
       on:keydown={(e) => {
@@ -70,7 +69,7 @@
 
 <style lang="scss">
   .numeric-input {
-    display: flex;
+    font-family: 'Inconsolata', monospace;
   }
 
   .chunk {
