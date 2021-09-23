97cf73ac3776108668b6a735fafc2568300896aa
johannchopin <johannchopin@pm.me>
2021-09-23 16:01:27 +0200

    💩 Render some AlphanumericInput for preview

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,6 +2,8 @@
   import { _ } from 'svelte-i18n'
   import NextButton from '../components/NextButton.svelte'
 
+  import QuestionsForm from '../components/QuestionsForm/QuestionsForm.svelte'
+  import AlphanumericInput from '../components/AlphanumericInput/AlphanumericInput.svelte'
   const PARAGRAPH_AMOUNT = 6
   const I18N_KEYS = 'home.paragraph_'
   const PARAGRAPHS_KEYS = Array(PARAGRAPH_AMOUNT).fill(undefined).map((__, i) => `${I18N_KEYS}${i + 1}`)
@@ -14,3 +16,14 @@
 <NextButton>
   { $_('continue') }
 </NextButton>
+<AlphanumericInput
+  value="T890SOLA6RF6628GB8"
+  chunkLength={4}
+  onSuccess={() => alert('success')}
+/>
+<AlphanumericInput
+  value="T890SOLA6RF6628GB8"
+  chunkLength={2}
+  onSuccess={() => alert('success')}
+/>
+
