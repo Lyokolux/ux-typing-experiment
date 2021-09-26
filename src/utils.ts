33dc6cab03b96242c84bd428fb29e7f3d53831bf
Lyokolux <lyokolux@protonmail.com>
2021-09-26 10:44:18 +0200

    Update src/utils.ts
    Co-authored-by: johannchopin <31794680+johannchopin@users.noreply.github.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export const getChunk = (str: string, size: number): string[] => {
-  return str.replace(' ', '').match(new RegExp('.{1,' + size + '}', 'g'))
+  return str.match(new RegExp('.{1,' + size + '}', 'g'))
 }
 
 export const reverseArray = <T>(arr: T[]): T[] => {
