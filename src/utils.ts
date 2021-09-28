3cf1976f40432bf2245057d944426485da0498a5
johannchopin <johannchopin@pm.me>
2021-09-28 22:21:38 +0200

    🚚 Move getGeneratedRandomString to utils

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,9 @@ export const getChunk = (str: string, size: number): string[] => {
 export const reverseArray = <T>(arr: T[]): T[] => {
   return [].concat(arr).reverse()
 }
+
+// from https://1loc.dev/ → Generate a random string with given length
+export const getGeneratedRandomString = (length = 18): string => Array(length).fill('').map(() => {
+  return Math.random().toString(36).charAt(2)
+}).join('')
+  .toUpperCase()
