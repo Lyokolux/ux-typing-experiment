fd9da86554d3b51f2435e845fdc32e6e8d7cc968
johannchopin <johannchopin@pm.me>
2021-09-28 23:45:58 +0200

    💡 Add TODOS for experience storage in firestore

--- src/components/Experience.svelte
+++ src/components/Experience.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { ChunkLength, EXPERIENCES_AMOUNT } from '../const'
 
+  import type { Event } from '../utils'
+
+  import { swiper } from '../stores';
   import AlphanumericDisplay from './AlphanumericDisplay.svelte'
   import AlphanumericInput from './AlphanumericInput/AlphanumericInput.svelte'
 
@@ -8,6 +11,16 @@
   export let displayChunkLength: ChunkLength
   export let inputChunkLength: ChunkLength
   export let currentIndex: number
+
+  let events: Event[]
+
+  const onFilled = (): void => {
+    $swiper.slideNext()
+    // TODO: Store events in firestore
+  }
+  const onCancel = (): void => {
+    // TODO: smthg
+  }
 </script>
 
 <h2 class="mt-5">{currentIndex + 1}/{EXPERIENCES_AMOUNT}</h2>
@@ -17,7 +30,8 @@
 />
 <AlphanumericInput 
   {value}
+  bind:events
   chunkLength={inputChunkLength}
-  onFilled={() => console.log('filled')}
-  onCancel={() => console.log('cancel')}
+  {onFilled}
+  {onCancel}
 />
