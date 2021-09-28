35b2ef0e1e96ac092ef4b3bb78aca5a7a7f7be2c
Lyokolux <lyokolux@protonmail.com>
2021-09-28 20:07:02 +0200

    ✨(api.ts): implement first request addUser

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,22 +1,10 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
   import { _ } from 'svelte-i18n'
-  import { collection, getDocs } from 'firebase/firestore/lite'
-
-  import { initFirestore } from '../firestore'
   import NextButton from '../components/NextButton.svelte'
 
   const PARAGRAPH_AMOUNT = 6
   const I18N_KEYS = 'home.paragraph_'
   const PARAGRAPHS_KEYS = Array(PARAGRAPH_AMOUNT).fill(undefined).map((__, i) => `${I18N_KEYS}${i + 1}`)
-
-  onMount(() => {
-    const { db } = initFirestore()
-
-    getDocs(collection(db, 'users')).then(value => {
-      console.log(value.docs.map(doc => doc.data()))
-    })
-  })
 </script>
 
 <h1>{$_('title')}</h1>
---
+++ src/store/api.ts
@@ -0,0 +1,37 @@
+import { initFirestore } from '../firestore'
+import {
+  arrayUnion,
+  collection,
+  CollectionReference,
+  doc,
+  DocumentReference,
+  updateDoc,
+  getDocs
+} from 'firebase/firestore'
+
+function createApi() {
+  const { db } = initFirestore()
+  const usersCollection = collection(db, 'users')
+
+  const fetchTheMostRecentDocument = async <T>(inCollection: CollectionReference<T>): Promise<DocumentReference<T>> => {
+    const { docs } = await getDocs(inCollection)
+    if (docs.length > 0) {
+      return docs[docs.length - 1].ref
+    }
+    return doc(inCollection, new Date().toString())
+  }
+
+  const addUserRequest = async (user) => {
+    // TODO: type to UserInfo
+    const document = await fetchTheMostRecentDocument<any>(usersCollection)
+    return updateDoc(document, {
+      users: arrayUnion(user)
+    })
+  }
+
+  return {
+    addUserRequest
+  }
+}
+
+export const api = createApi()
