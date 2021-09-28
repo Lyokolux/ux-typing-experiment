4bac741f83109985bd5de0da35d71acc0da9f8d8
johannchopin <johannchopin@pm.me>
2021-09-28 23:45:06 +0200

    ✨ Bind PostExperience questions

--- src/pages/Experiences.svelte
+++ src/pages/Experiences.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
   import { getExperiencesConfigs } from '../utils'
 
+  import type { Question } from '../components/QuestionsForm/QuestionsForm.svelte'
+
+  import { swiper } from '../stores';
   import Page from '../components/Page.svelte'
   import Experience from '../components/Experience.svelte'
   import PostExperience from './PostExperience.svelte'
 
   const experiences = getExperiencesConfigs()
+
+  let questions: Question[][] = []
+
+  const onFormSubmit = (questionIndex: number): void => {
+    $swiper.slideNext()
+    console.log(questions[questionIndex])
+  }
 </script>
 
 {#each experiences as { value, displayChunkLength, inputChunkLength}, i}
@@ -18,6 +28,9 @@
     />
   </Page>
   <Page>
-    <PostExperience />
+    <PostExperience 
+      onSubmit={() => onFormSubmit(i)}
+      bind:questions={questions[i]} 
+    />
   </Page>
 {/each}
