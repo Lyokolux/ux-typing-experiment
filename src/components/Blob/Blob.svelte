22cc0b9918521ee6573bb2028a3ed7a48d0ee364
johannchopin <johannchopin@pm.me>
2021-09-30 13:12:59 +0200

    ✨ Add Blob component

---
+++ src/components/Blob/Blob.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import EggOutline from './blobs/egg-outline.svelte'
+  import StackedWaves from './blobs/stacked-waves.svelte'
+
+
+  let className = ''
+  export { className as class }
+
+  type Blob = 'egg-outline' | 'stacked-waves'
+  export let type: Blob = 'egg-outline' 
+
+  const BLOBS: {[blobType in Blob]: any} = {
+    'egg-outline': EggOutline,
+    'stacked-waves': StackedWaves
+  }
+</script>
+
+<i class={`blob ${type} ${className}`}>
+  <svelte:component this={BLOBS[type]} />
+</i>
+
+<style>
+  i :global(svg) {
+    height: 100%;
+    width: 100%;
+  }
+</style>
