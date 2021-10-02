e52bed1b20f4925c9e075b2859f4acb2fbcd297c
johannchopin <johannchopin@pm.me>
2021-10-02 10:27:58 +0200

    ✨ Add prop id to Page

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  /* eslint-disable no-use-before-define */
+  export let id: string = undefined
   let className = ''
   export { className as class }
 </script>
-<div class={`page swiper-slide h-100 w-100 container ${className}`}><slot></slot></div>
+<div class={`page swiper-slide h-100 w-100 container ${className}`} {id}><slot></slot></div>
