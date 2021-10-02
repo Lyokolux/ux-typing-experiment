2ca72a77ddb039829c20de5f49072eb103451d9d
johannchopin <johannchopin@pm.me>
2021-10-02 12:07:38 +0200

    💄Use fade transition

--- src/components/LoadingScreen.svelte
+++ src/components/LoadingScreen.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { fly } from 'svelte/transition'
+  import { fade } from 'svelte/transition'
 
   export let visible: boolean
 </script>
@@ -7,7 +7,7 @@
 {#if visible}
   <div 
     class="loading-screen position-absolute h-100 w-100 bg-background d-flex align-items-center justify-content-center"
-    out:fly={{ y: -200, duration: 500 }}
+    out:fade={{ duration: 500 }}
   >
     <div class="spinner-border" role="status">
       <span class="visually-hidden">Loading...</span>
