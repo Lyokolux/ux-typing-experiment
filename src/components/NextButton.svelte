ac0479cd314058ce34931277be9ac59d20831b40
Lyokolux <lyokolux@protonmail.com>
2021-10-03 17:26:46 +0200

    ♻️(NextButton.svelte): use className

--- src/components/NextButton.svelte
+++ src/components/NextButton.svelte
@@ -1,9 +1,12 @@
-<div class="row justify-center">
-    <button class="btn btn-primary my-auto" on:click={() => $swiper.slideNext() }>
+<div class="d-flex">
+    <button class={`btn btn-primary my-auto ${className}`} on:click={() => $swiper.slideNext() }>
         <slot></slot>
     </button>
 </div>
 
 <script>
     import { swiper } from '../stores'
+
+    let className = ""
+    export { className as class }
 </script>
