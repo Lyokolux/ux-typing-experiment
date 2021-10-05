4b3388a51214f0219b5252acc86fcebea182a3ea
Lyokolux <lyokolux@protonmail.com>
2021-10-05 23:44:40 +0200

    🐛(NextButton.svelte): add callback

--- src/components/NextButton.svelte
+++ src/components/NextButton.svelte
@@ -1,13 +1,14 @@
 <div class={`d-flex my-5 ${className}`}>
-    <button class="btn btn-primary my-auto" on:click={() => $swiper.slideNext() }>
+    <button class="btn btn-primary my-auto" on:click={() => { $swiper.slideNext(); onClick() } }>
         <slot>{$_('continue')}</slot>
     </button>
 </div>
 
-<script>
+<script lang="ts">
     import { _ } from 'svelte-i18n'
     import { swiper } from '../stores'
 
     let className = ''
     export { className as class }
+    export let onClick: () => void  = () => {};
 </script>
