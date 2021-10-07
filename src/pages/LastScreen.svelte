1c9de1a785e1c3af9ee11eb4b3d56ee9c81e8213
johannchopin <johannchopin@pm.me>
2021-10-07 15:20:27 +0200

    ✨ Render all devs

--- src/pages/LastScreen.svelte
+++ src/pages/LastScreen.svelte
@@ -1,7 +1,46 @@
 <script lang="ts">
-    import { _ } from 'svelte-i18n'
+  import { _ } from 'svelte-i18n'
+
+  interface Dev {
+    name: string
+    link: string
+  }
+
+  const DEVS: Dev[] = [
+    {
+      name: '@johannchopin',
+      link: 'https://github.com/johannchopin'
+    },
+    {
+      name: '@Alexandre Guidoux',
+      link: 'https://github.com/Lyokolux/ux-typing-experiment'
+    },
+    {
+      name: '@Florian Weiss',
+      link: 'https://fweiss.com/'
+    },
+    {
+      name: '@ibrahimlahouar',
+      link: 'https://github.com/ibrahimlahouar'
+    }
+  ]
 </script>
 
-<p>{$_('last_screen.thanks')}</p>
+<h2>{$_('last_screen.thanks')}</h2>
 
-<p>{$_('last_screen.contact')}</p>
+<p class="mt-5">
+  {@html $_('last_screen.contact')}
+</p>
+
+<p class="mt-5">
+  {@html $_('last_screen.produced_by')}
+</p>
+
+<ul class="list-group list-group-flush w-fit-content">
+  {#each DEVS as { name, link }}
+    <li class="list-group-item bg-transparent">
+      <a href={link} target="_blank">{name}</a>
+    </li>
+  {/each}
+</ul>
+
