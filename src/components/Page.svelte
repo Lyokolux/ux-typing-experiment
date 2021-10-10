e480f5cb6b1423f31ae28430af556673a485b3ff
johannchopin <johannchopin@pm.me>
2021-10-10 14:18:09 +0200

    🩹 Create copy for BLOBS_ARRANGEMENT_IDS

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -13,7 +13,7 @@
 <div class={`page swiper-slide position-relative d-flex justify-content-center h-100 w-100 m-0 ${className}`} {id}>
   <section class="mx-1 mx-md-5"><slot></slot></section>
 
-  <BlobsArrangement type={getRandomInArray(BLOBS_ARRANGEMENT_IDS)}/>
+  <BlobsArrangement type={getRandomInArray([...BLOBS_ARRANGEMENT_IDS])}/>
 </div>
 
 <style lang="scss">
