fea3507c2ab8b994a9645e1b412db9e240be4b47
Lyokolux <lyokolux@protonmail.com>
2021-10-11 14:44:38 +0200

    ✨(LoadingScreen.svelte): add a loader with input

--- src/components/LoadingScreen.svelte
+++ src/components/LoadingScreen.svelte
@@ -1,16 +1,39 @@
 <script lang="ts">
   import { fade } from 'svelte/transition'
+  import { _ } from 'svelte-i18n'
+  import { CHUNK_SIZES } from '../const'
+  import { getRandomInArray } from '../utils'
 
   export let visible: boolean
+
+  const AMOUNT_OF_ITEMS = 4
+  let inputSizes = Array(AMOUNT_OF_ITEMS).fill(undefined).map(() => getRandomInArray(CHUNK_SIZES))
+
+  let intervalId = setInterval(() => {
+    inputSizes = Array(AMOUNT_OF_ITEMS).fill(undefined).map(() => getRandomInArray(CHUNK_SIZES))
+  }, 1000)
+
+  $: {
+    if(!visible) {
+      clearInterval(intervalId)
+    }
+  }
 </script>
 
 {#if visible}
   <div 
-    class="loading-screen position-absolute h-100 w-100 bg-background d-flex align-items-center justify-content-center"
+    class="loading-screen position-absolute h-100 w-100 bg-background align-items-center justify-content-center"
     out:fade={{ duration: 500 }}
   >
-    <div class="spinner-border" role="status">
-      <span class="visually-hidden">Loading...</span>
+    <h1 class="mb-3 text-center">{ $_('loading') }</h1>
+    <div class="d-flex row">
+      {#each inputSizes as element, i}
+        <input
+          type="text"
+          class={`alph anumeric-input chunk font-digit-readable form-control text-center fs-4 p-0 p-md-2 fw-bold`}
+          style={`--width: ${element / 1.7}em`}
+        />
+      {/each}
     </div>
   </div>
 {/if}
@@ -18,5 +41,12 @@
 <style>
   .loading-screen {
     z-index: 101;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .chunk {
+    width: var(--width);
+    transition: width ease-out 0.6s;
   }
 </style>
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
 import {
   ALPHANUMERIC_LENGTH,
-  ALPHANUMERIC_REGEX,
   CHUNK_SIZES,
   DESKTOP_SCREEN_MIN_WIDTH,
   NON_ALPHANUMERIC_REGEX
@@ -47,7 +46,7 @@ export const getShuffledArray = <T>(array: T[]): T[] => {
   return array
 }
 
-export const getRandomInArray = <T>(array: T[]): T => {
+export const getRandomInArray = <T extends readonly any[]>(array: T): T[number] => {
   const randomIndex = Math.floor(Math.random() * array.length)
   return array[randomIndex]
 }
@@ -116,3 +115,7 @@ export const getFilteredExperiencesByChunkSize = (
       : experiment.id.startsWith(chunkSize.toString())
   })
 }
+
+export const getRandomAlphaCharacter = (amount: number):string => (
+  Math.random().toString(36).slice(amount)
+)
