87ad0256ecbd6b5816dae26826663d1d8f479297
johannchopin <johannchopin@pm.me>
2021-10-18 17:41:39 +0200

    ✨ Create results page and prefetch users

---
+++ src/routes/results/index.svelte
@@ -0,0 +1,19 @@
+<script context="module" lang="ts">
+  import { api, FirestoreStoredUser } from '../../stores/api'
+
+  export async function load() {
+    const users = await api.getAllUsers()
+
+    return {
+      props: {
+        users
+      }
+    }
+  }
+</script>
+
+<script lang="ts">
+  export let users: FirestoreStoredUser[]
+</script>
+
+<h1>{users.length}</h1>
